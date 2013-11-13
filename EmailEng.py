9d3abe124b3d584b09da9377a9169b7e2a7d187b
Joseph Livecchi <joewashear007@gmail.com>
2013-11-12 20:13:14 -0800

    The start of an email class

--- EmailEng.py
+++ EmailEng.py
@@ -1,16 +1,33 @@
 #!/usr/bin/python
-import imaplib
-import datetime
+
+# Import smtplib for the actual sending function
+import smtplib
 import email
-import email.header
-import pprint
-import quopri
-import re
-import HTMLParser
+# Import the email modules we'll need
+from email.mime.text import MIMEText
+
 
 class EmailEng:
 
-	def __init__():
+	def __init__(self):
+		user = ""
+		pw = ""
 		
-	def sendTextMsg(msg):
+	def sendTextMsg(self, msg):
+		# Create a text/plain message
+		msg = email.mime.text.MIMEText(msg)
+		msg['Subject'] = "The test email"
+		msg['From'] = "joesboxoftricks.com"
+		msg['To'] = "5672772237@vtext.com"
+
+		# Send the message via our own SMTP server.
+		s = smtplib.SMTP('localhost')
+		s.send_message(msg)
+		s.quit()
 		
+def main():
+	e = EmailEng()
+	e.sendTextMsg("Hello World")
+	
+if __name__ == '__main__':
+    main()
